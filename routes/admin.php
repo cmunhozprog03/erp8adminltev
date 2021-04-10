960c2ff84903bec1e157bdf64a5fb7514d378268
cmunhozprog03h <cmunhozprog03@hotmail.com>
2021-04-10 08:29:19 -0300

    instalação e configuração do adminlte

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('', function(){
-  return "Hola Administrador";
-});
+Route::get('', [HomeController::class, 'index']);
+
